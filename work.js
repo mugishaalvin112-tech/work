e0d7c3dfdee88b984bf64a890f1dde8838246f4c
mugishaalvin112-tech <mugishaalvin112@gmail.com>
2026-05-11 12:26:46 +0200

    Add files via upload

--- work.js
+++ work.js
@@ -2,142 +2,40 @@ const express = require("express");
 const mongoose = require("mongoose");
 
 const app = express();
-
 app.use(express.json());
 
+mongoose.connect("mongodb://localhost:27017/crud_db");
 
-// CONNECT TO MONGODB
-mongoose.connect("mongodb://127.0.0.1:27017/crud_db")
-.then(() => console.log("MongoDB Connected"))
-.catch((err) => console.log(err));
-
-
-// ITEM MODEL
 const Item = mongoose.model("Item", {
-    name: String,
-    description: String,
-    price: Number
+  name: String,
+  description: String,
+  price: Number
 });
 
-
-
-// GET ALL ITEMS
+// Get all
 app.get("/items", async (req, res) => {
-
-    const items = await Item.find();
-
-    res.status(200).json(items);
+  res.json(await Item.find());
 });
 
-
-
-// GET ONE ITEM
+// Get one
 app.get("/items/:id", async (req, res) => {
-
-    try {
-
-        const item = await Item.findById(req.params.id);
-
-        if (!item) {
-            return res.status(404).json({
-                message: "Item not found"
-            });
-        }
-
-        res.status(200).json(item);
-
-    } catch {
-
-        res.status(500).json({
-            message: "Error"
-        });
-    }
+  res.json(await Item.findById(req.params.id));
 });
 
-
-
-// CREATE ITEM
+// Create
 app.post("/items", async (req, res) => {
-
-    const { name, description, price } = req.body;
-
-    if (!name || !description || !price) {
-
-        return res.status(400).json({
-            message: "All fields required"
-        });
-    }
-
-    const item = new Item({
-        name,
-        description,
-        price
-    });
-
-    await item.save();
-
-    res.status(201).json(item);
+  res.json(await Item.create(req.body));
 });
 
-
-
-// UPDATE ITEM
+// Update
 app.put("/items/:id", async (req, res) => {
-
-    try {
-
-        const item = await Item.findByIdAndUpdate(
-            req.params.id,
-            req.body,
-            { new: true }
-        );
-
-        if (!item) {
-
-            return res.status(404).json({
-                message: "Item not found"
-            });
-        }
-
-        res.status(200).json(item);
-
-    } catch {
-
-        res.status(500).json({
-            message: "Error"
-        });
-    }
+  res.json(await Item.findByIdAndUpdate(req.params.id, req.body, { new: true }));
 });
 
-
-
-// DELETE ITEM
+// Delete
 app.delete("/items/:id", async (req, res) => {
-
-    try {
-
-        const item = await Item.findByIdAndDelete(req.params.id);
-
-        if (!item) {
-
-            return res.status(404).json({
-                message: "Item not found"
-            });
-        }
-
-        res.status(204).send();
-
-    } catch {
-
-        res.status(500).json({
-            message: "Error"
-        });
-    }
+  res.json(await Item.findByIdAndDelete(req.params.id));
 });
 
-
-
-// START SERVER
-app.listen(5000, () => {
-    console.log("Server running on port 5000");
-});
+// Start server
+app.listen(3000, () => console.log("Server running on 3000"));
